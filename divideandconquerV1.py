399b1dd871d4feb84276d354abf01f872a648333
kenny14231 <37006870+kenny14231@users.noreply.github.com>
2021-03-07 16:27:43 -0500

    Update divideandconquerV1.py

--- divideandconquerV1.py
+++ divideandconquerV1.py
@@ -83,35 +83,11 @@ def divideAndConquer2(array:list):
     leftBestPosition:list = divideAndConquerIndex(leftArray)
     rightBestPosition:list = divideAndConquerIndex(rightArray)
 
-    leftBestArray:list = array[leftBestPosition[0]: leftBestPosition[1]]
-    rightBestArray:list = array[rightBestPosition[0]+halfwayPoint: rightBestPosition[1]+halfwayPoint]
-    inBetweenArray:list = array[leftBestPosition[1]: rightBestPosition[0]+halfwayPoint]
+    newArray:list = array[leftBestPosition[0]:rightBestPosition[1]+halfwayPoint]
+    maxArrayPosition:list = divideAndConquerIndex(newArray)
+    maxArray:list = newArray[maxArrayPosition[0]:maxArrayPosition[1]]
 
-    #print(f"leftBestArray: {leftBestArray}")
-    #print(f"inBetweenArray: {inBetweenArray}")
-    #print(f"rightBestArray: {rightBestArray}")
-
-    leftBestSum:int = sum(leftBestArray)
-    rightBestSum:int = sum(rightBestArray)
-    inBetweenSum:int = sum(inBetweenArray)
-
-    if(leftBestSum >= rightBestSum):
-        maxArray = leftBestArray
-        if(inBetweenSum + rightBestSum > 0 and rightBestSum > 0):
-            #add inbetweensum and rightbestsum to array
-            maxArray = maxArray + inBetweenArray + rightBestArray
-        elif(inBetweenSum > 0 and rightBestSum < 0):
-            #add inbetweensum to array
-            maxArray = maxArray + inBetweenArray
-    else:
-        maxArray = rightBestArray
-        if(inBetweenSum + leftBestSum > 0 and leftBestSum > 0):
-            #add inbetweensum and rightbestsum to array
-            maxArray = leftBestArray + inBetweenArray + maxArray
-        elif(inBetweenSum > 0 and rightBestSum < 0):
-            #add inbetweensum to array
-            maxArray = inBetweenArray + maxArray
-    return (maxArray, inBetweenArray)
+    return (maxArray, newArray, rightArray)
 
 def main():
     times1 :list = []
@@ -121,55 +97,33 @@ def main():
     #for size in range(1, 10):
         #arraySize:int = size * 30
     arraySize:int = 100
+    allGood = True
 
-    for i in range(20):
+    for i in range(100):
         newArray :list = createArray(arraySize)
         
         maxSubArray1 = divideAndConquer1(newArray)[0]
 
         result2 = divideAndConquer2(newArray)
         maxSubArray2 = result2[0]
-        inBetweenArray = result2[1]
+        cutArray = result2[1]
+        rightArray = result2[2]
 
+        
+        
         if(maxSubArray1 != maxSubArray2):
+            allGood = False
             print("discrepancy!")
             print(f"newArray: {newArray}")
             print(f"divideAndConquer1 bestArray: {maxSubArray1}")
             print(f"divideAndConquer2 bestArray: {maxSubArray2}")
-            print(f"divideAndConquer2 inBetweenArray: {inBetweenArray}")
+            print(f"divideAndConquer2 cutArray: {cutArray}")
+            print(f"divideAndConquer2 rightArray: {rightArray}")
+            print("\n")
+
+    print(f"allGood: {allGood}")
         #print(f"Are they equivalent? {maxSubArray1 == maxSubArray2}")
-        
-
-    for i in range(listLength):
-        newArray :list = createArray(arraySize)
-        print(f"newArray: {newArray}")
-
-        result1:tuple = divideAndConquer(newArray)
-        maxSubArray1 :list = result[0]
-        maxSubArraySum1 = sum(maxSubArray)
-        print(f"maxSubArray1: {maxSubArray1}")
-        print(f"maxSum1: {maxSubArraySum1}")
-
-        timeTaken1 :float = result[1]
-        times1.append(timeTaken1)
-
-        result2:tuple = divideAndConquer2(newArray)
-        maxSubArray2:list = result2[0]
-        maxSubArraySum2 = sum(maxSubArray2)
-        print(f"maxSubArray2: {maxSubArray2}")
-        print(f"maxSum2: {maxSubArraySum2}")
-
-        timeTaken2 :float = result2[1]
-        times2.append(timeTaken2)
-        
-
-    averageTime1 :float = sum(times1) / listLength
-    #print(f"arraySize: {arraySize}")
-    print(f"Average Time 1: {averageTime1}")
-
-    averageTime1 :float = sum(times2) / listLength
-    #print(f"arraySize: {arraySize}")
-    print(f"Average Time 2: {averageTime2}")
+       
 
 if __name__ == "__main__":
     main()
